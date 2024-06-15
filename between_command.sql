858a67726cf0f60d4d910814f4c289a7157369e3
Gabriela Karkowska <karkowskagabriela@gmail.com>
2024-06-15 15:31:11 +0200

    between command

---
+++ between_command.sql
@@ -0,0 +1,12 @@
+-- You are given a table of persons which includes their respective age. Your task is to return the list of people, age 30 through 50, using the BETWEEN keyword.
+-- The schema for the persons table is as follows:
+-- id
+-- name
+-- age
+-- The resulting table should have two columns as follows:
+
+-- name
+-- age
+-- Order by age, highest to lowest.
+
+SELECT name, age FROM persons WHERE age BETWEEN 30 AND 50 ORDER BY age DESC
