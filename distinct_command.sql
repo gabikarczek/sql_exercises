0d02e9aa2d726ea6b69eefd8e4a0d12624ec955c
Gabriela Karkowska <karkowskagabriela@gmail.com>
2024-06-16 14:16:38 +0200

    distinct command exercise

---
+++ distinct_command.sql
@@ -0,0 +1,15 @@
+-- For this challenge you need to create a simple DISTINCT statement, you want to find all the unique ages.
+
+--  table schema
+-- id
+-- name
+-- age
+-- select table schema
+-- age (distinct)
+-- NOTE: Your solution should use pure SQL. Ruby is used within the test cases to do the actual testing.
+
+
+SELECT DISTINCT age FROM people 
+
+-- as a preference, you could also add ORDER BY age DESC just to make the output more readable :)
+
