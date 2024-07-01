4e68770b445904c1adb10d0deed0e6d87ffb6d9a
gabikarczek <164886279+gabikarczek@users.noreply.github.com>
2024-07-01 19:19:41 +0200

    update debugging.sql

--- debugging.sql
+++ debugging.sql
@@ -1,20 +1,22 @@
--- Oh no! Timmys been moved into the database divison of his software company but as we know Timmy loves making mistakes. Help Timmy keep his job by fixing his query...
+/*
+Oh no! Timmys been moved into the database divison of his software company but as we know Timmy loves making mistakes. Help Timmy keep his job by fixing his query...
 
--- Timmy works for a statistical analysis company and has been given a task of totaling the number of sales on a given day grouped by each department name and then each day.
+Timmy works for a statistical analysis company and has been given a task of totaling the number of sales on a given day grouped by each department name and then each day.
 
--- SELECT 
- --  s.transaction_date as day,
-  -- d.name,
-  -- COUNT(s.id)
-  -- FROM department d
-   --  JOIN sale s on d.id = s.id
-  -- group by day, d.name
-  -- order by name desc
+SELECT 
+s.transaction_date as day,
+d.name,
+COUNT(s.id)
+FROM department d
+JOIN sale s on d.id = s.id
+group by day, d.name
+order by name desc
 
--- Resultant table:
--- day (type: date) {group by} [order by asc]
--- department (type: text) {group by} [In a real world situation it is bad practice to name a column after a table]
--- sale_count (type: int)
+Resultant table:
+day (type: date) {group by} [order by asc]
+department (type: text) {group by} [In a real world situation it is bad practice to name a column after a table]
+sale_count (type: int)
+*/
 
 SELECT
   DATE(sale.transaction_date) as day,
